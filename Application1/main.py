98873f1cee0a418d4f3b07e4ad1d85226cd2549f
Lee <123798682+Calmcat1@users.noreply.github.com>
2023-11-13 15:00:24 +0300

    Update main.py

--- Application1/main.py
+++ Application1/main.py
@@ -6,6 +6,6 @@ app = FastAPI()
 
 @app.get('/data/Application/v1')
 async def get_json_data():
-  with open('../Data/testData.json', 'r') as json_file:
+  with open('Data/testData.json', 'r') as json_file:
     json_data = json_file.read()
   return json.loads(json_data)
